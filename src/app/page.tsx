8f0624e5dd98c5512dd30505260aba1b3469f963
Raul <raulnascimento.contato@gmail.com>
2026-06-03 11:51:43 -0300

    refactor Home component layout and structure for improved organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
+import { AreasDeAtuacao } from "@/components/layout/areasDeAtuacao";
+import { ComoFunciona } from "@/components/layout/comoFunciona";
+import { ContatoFormulario } from "@/components/layout/contatoFormulario";
+import { Depoimentos } from "@/components/layout/depoimentos";
+import { FAQ } from "@/components/layout/faq";
 import { Footer } from "@/components/layout/footer";
-import { Header } from "@/components/layout/header"
+import { Header } from "@/components/layout/header";
+import { Sobre } from "@/components/layout/sobre";
+import { Hero } from "@/components/sections/hero";
 
 export default function Home() {
   return (
-    <>
+    <div className="min-h-screen">
       <Header />
-      <h1>Main page</h1>
+      <main>
+        <AreasDeAtuacao />
+        <Sobre />
+        <ComoFunciona />
+        <Depoimentos />
+        <FAQ />
+        <ContatoFormulario />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
